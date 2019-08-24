6df6eb81d88f8bcd696d4b8c970049168c3d13eb
Rahul Makhija <rahuls360@gmail.com>
2019-08-24 21:29:46 +0530

    add router for crud operation

---
+++ src/components/Operation.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import Axios from 'axios';
+
+//http://dummy.restapiexample.com/ use this api
+
+class Operation extends React.Component<any, any> {
+
+  constructor(props: any) {
+    super(props);
+    this.state = {
+      data: null
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        hi
+      </div>
+    );
+  }
+}
+
+export default Operation;
